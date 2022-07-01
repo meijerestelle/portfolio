179a59a7ff42d2e40f50401e69802063cdcfa791
Estelle <66007150+meijerestelle@users.noreply.github.com>
2022-07-01 23:47:17 +0200

    case template & hover states on home

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,15 @@ module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
         extend: {
+            aspectRatio: {
+              '2/1': '2 / 1',
+            },
             fontFamily: {
                 'manrope' : ['Manrope', 'sans-serif'],
                 'inter' : ['Inter', 'sans-serif'],
             }
         },
+
         colors: {
             stone: colors.stone,
             'white': '#f8f8f8',
